172b3d18fb17c551cbba8c77acb0abc3ed70f8f4
leeforaday <leonorflag@hotmail.com>
2015-05-18 17:56:39 +0000

    cenasss

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -1,5 +1,6 @@
 // $Id: postfix_writer.cpp,v 1.7 2015/04/14 10:00:27 ist173639 Exp $ -*- c++ -*-
 #include <string>
+#include <string.h>
 #include <sstream>
 #include "targets/type_checker.h"
 #include "targets/postfix_writer.h"
@@ -154,20 +155,38 @@ void pwn::postfix_writer::do_assignment_node(pwn::assignment_node * const node, 
 
 //---------------------------------------------------------------------------
 
-/*void pwn::postfix_writer::do_program_node(pwn::program_node * const node, int lvl) {
+void pwn::postfix_writer::do_func_def_node(pwn::func_def_node * const node, int lvl) {
   // Note that Simple doesn't have functions. Thus, it doesn't need
   // a function node. However, it must start in the main function.
   // The ProgramNode (representing the whole program) doubles as a
   // main function node.
 
   // generate the main function (RTS mandates that its name be "_main")
+  // stackcounter sc(node, _compiler);
+  CHECK_TYPES(_compiler, _symtab, node);
+  
   _pf.TEXT();
   _pf.ALIGN();
-  _pf.GLOBAL("_main", _pf.FUNC());
-  _pf.LABEL("_main");
+  
+  std::string * name = node->name()->name(); //nome da funcao
+  if(strcmp(name->c_str(), "pwn")==0){
+    name = new std::string("_main");
+  }
+  else if(strcmp(name->c_str(), "_main")==0){
+    name = new std::string("._main");
+  }
+  
+  const std::string &sName = *name;
+  _pf.GLOBAL(sName, _pf.FUNC());
+  _pf.LABEL(sName);
   _pf.ENTER(0);  // Simple doesn't implement local variables
 
-  node->statements()->accept(this, lvl);
+  if(node->name()->arguments() != NULL)
+    node->name()->arguments()->accept(this, lvl); //argumentos da funcao
+  if(node->instructions() != NULL)
+    node->instructions()->accept(this, lvl); //corpo da funcao
+  if(node->value() != NULL)
+    node->value()->accept(this, lvl); //retorno por omissao da funcao
 
   // end the main function
   _pf.INT(0);
@@ -180,7 +199,7 @@ void pwn::postfix_writer::do_assignment_node(pwn::assignment_node * const node, 
   _pf.EXTERN("printi");
   _pf.EXTERN("prints");
   _pf.EXTERN("println");
-}*/
+}
 
 //---------------------------------------------------------------------------
 
@@ -214,7 +233,6 @@ void pwn::postfix_writer::do_print_node(pwn::print_node * const node, int lvl) {
     std::cerr << "ERROR: CANNOT HAPPEN!" << std::endl;
     exit(1);
   }
-  _pf.CALL("println"); // print a newline
 }
 
 //---------------------------------------------------------------------------
@@ -302,13 +320,54 @@ void pwn::postfix_writer::do_block_node(pwn::block_node * const node, int lvl) {
   //TODO
 }
 void pwn::postfix_writer::do_func_decl_node(pwn::func_decl_node * const node, int lvl) {
-  //TODO
-}
-void pwn::postfix_writer::do_func_def_node(pwn::func_def_node * const node, int lvl) {
-  //TODO
+  CHECK_TYPES(_compiler, _symtab, node);
+  
+  std::string * name = node->name(); //nome da funcao
+  if(strcmp(name->c_str(), "pwn")==0){
+    name = new std::string("_main");
+  }
+  else if(strcmp(name->c_str(), "_main")==0){
+    name = new std::string("._main");
+  }
+  
+  const std::string &sName = *name;
+  std::shared_ptr<pwn::symbol> symbol = _symtab.find(sName);
+  
+  if (symbol == NULL){
+    _symtab.insert(sName, std::make_shared<pwn::symbol> (node->type(), sName, -1));    
+  }
+  else {
+    throw sName + "redeclaration";
+  }
 }
 void pwn::postfix_writer::do_func_call_node(pwn::func_call_node * const node, int lvl) {
-  //TODO
+  CHECK_TYPES(_compiler, _symtab, node);
+  
+  // stackcounter sc(node, _compiler);
+  std::string * name = node->name(); //nome da funcao
+  const std::string &fName = * name;
+  if(node->arguments() != NULL){
+    node->arguments()->accept(this, lvl+1);
+  }
+  
+  _pf.CALL(fName);
+  //_pf.TRASH(sc.size());
+ 
+  if(strcmp(name->c_str(), "pwn")==0){
+    name = new std::string("_main");
+  }
+  else if(strcmp(name->c_str(), "_main")==0){
+    name = new std::string("._main");
+  }
+  const std::string &sName = * name;
+  std::shared_ptr<pwn::symbol> symbol = _symtab.find(sName);
+  
+  if( (symbol->type()->name() == basic_type::TYPE_INT) || (symbol->type()->name() == basic_type::TYPE_STRING) || (symbol->type()->name() == basic_type::TYPE_POINTER) )
+     _pf.PUSH();
+  
+  if(symbol->type()->name() == basic_type::TYPE_DOUBLE)
+     _pf.DPUSH();
+  
 }
 void pwn::postfix_writer::do_var_node(pwn::var_node * const node, int lvl) {
   //TODO
@@ -317,7 +376,21 @@ void pwn::postfix_writer::do_var_decl_node(pwn::var_decl_node * const node, int 
   //TODO
 }
 void pwn::postfix_writer::do_println_node(pwn::println_node * const node, int lvl) {
-  //TODO
+  CHECK_TYPES(_compiler, _symtab, node);
+  node->argument()->accept(this, lvl); // determine the value to print
+  if (node->argument()->type()->name() == basic_type::TYPE_INT) {
+    _pf.CALL("printi");
+    _pf.TRASH(4); // delete the printed value
+  }
+  else if (node->argument()->type()->name() == basic_type::TYPE_STRING) {
+    _pf.CALL("prints");
+    _pf.TRASH(4); // delete the printed value's address
+  }
+  else {
+    std::cerr << "ERROR: CANNOT HAPPEN!" << std::endl;
+    exit(1);
+  }
+  _pf.CALL("println"); // print a newline
 }
 void pwn::postfix_writer::do_maloc_node(pwn::maloc_node * const node, int lvl) {
   //TODO
